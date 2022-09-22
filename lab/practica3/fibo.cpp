adaf56e4261146ae208f2e51ead67edb1d06e161
Alvaro <ahmamani@ucsp.edu.pe>
2022-09-22 08:30:45 -0500

    fib

---
+++ lab/practica3/fibo.cpp
@@ -0,0 +1,29 @@
+/**
+ * Implementar dos funciones, una iterativa y otra recursiva, 
+ * que reciba un número entero n y retorne el n-ésimo fibonacci.
+ * 
+ * fib(0) = 1
+ * fib(1) = 1
+ * fib(2) = fib(1) + fib(0) = 1 + 1 = 2
+ * fib(3) = fib(2) + fib(1) = 2 + 1 = 3
+ * fib(4) = fib(3) + fib(2) = 3 + 2 = 5
+ * ...
+ * fib(n) = fib(n-1) + fib(n-2)
+*/
+#include <iostream>
+using namespace std;
+
+int fib_rec(int n) {
+    // ...
+}
+
+int fib_iter(int n) {
+    // ...
+}
+
+int main() {
+    cout << fib_rec(5) << endl;
+    cout << fib_iter(5) << endl;
+
+    return 0;
+}
